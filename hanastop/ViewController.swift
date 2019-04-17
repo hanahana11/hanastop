5e3a2c47bd51d36aa75b568bbc1c9ca5ecc7e7e6
hanahana11 <hana87kotaro@i.softbank.jp>
2019-04-17 19:26:11 +0900

    リリースでけて嬉しい

--- hanastop/ViewController.swift
+++ hanastop/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @objc var timer = Timer()
+    var startime: TimeInterval = 0
+    var elapseTime: Double = 0.0
+    var time : Double = 0.0
+    var count : Float = 0
+    
+    @IBOutlet weak var labelTimer: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    @objc func Timerup() {
+        count = count + 0.01
+        labelTimer.text = String(format: "%.2f", count)
+    }
+    
+        
+    
+    
+    @IBAction func start(){
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(Timerup),
+                                         userInfo: nil,
+                                         repeats: true)
+        }
+    }
+    
+    @IBAction func stop(){
+        if timer.isValid {
+            timer.invalidate()
+        }
     }
 
 
-}
 
+
+}
